4649fda7f17b20fdf7a9da05c4c5f20989305475
emsiri <emsirikami@gmail.com>
2016-03-07 11:29:43 +1000

    Changed to chrome to test

--- scripts/test.js
+++ scripts/test.js
@@ -12,12 +12,12 @@ function runChimp() {
   var childProcess = spawn('chimp',
      [
        '--watch',
-       '--browser=phantomjs',
+       '--browser=chrome',
        '--singleSnippetPerFile=1',
        '--jsonOutput=cucumber/chimpTests.cucumber',
        '--saveScreenshots=true',
-       '--attachScreenshotsToReport=true',
-       '--format=json'
+       '--attachScreenshotsToReport=true'
+      //  '--format=json'
       //  '--screenshotsOnError=true'
      ]
   );
